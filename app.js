bf42cc9bfe57e951be50da35e7852dcadbf0323c
Aditya Ladwa <ladwa.aditya@gmail.com>
2015-09-20 16:48:18 +0530

    Succefully recieving get request from contorller

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
 
-
-
+//Use static public directory
 app.use(express.static(__dirname + "/public"));
+//Handel get request for contact list
+app.get('/contactList',function(req,res){
+	console.log("I recieved a get requent");
 
+	person1  ={
+			name: "Aditya",
+			email: "ladwa.aditya@gmail.com",
+			number: "7411438334"
+		};
+		person2  ={
+			name: "Vishal",
+			email: "ladwa.vishal@gmail.com",
+			number: "7411438334"
+		};
+
+	var contactList = [person1,person2];
+	res.json(contactList);
+});
 var server = app.listen(3000,function(){
 	console.log("App listening at port 3000");
 });
--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,17 +1,10 @@
 var myApp =angular.module('myApp',[]);
 
-myApp.controller('AppCtrl', ['$scope', function($scope){
+myApp.controller('AppCtrl', ['$scope','$http', function($scope,$http){
 	console.log("hello from appController");
-	person1  ={
-		name: "Aditya",
-		email: "ladwa.aditya@gmail.com",
-		number: "7411438334"
-	};
-	person2  ={
-		name: "Vishal",
-		email: "ladwa.vishal@gmail.com",
-		number: "7411438334"
-	};
-	var contactList = [person1,person2];
-	$scope.contactList = contactList;
+
+	$http.get('/contactList').success(function(response){
+		console.log("I got the response");
+		$scope.contactList =response;
+	});
 }]);
